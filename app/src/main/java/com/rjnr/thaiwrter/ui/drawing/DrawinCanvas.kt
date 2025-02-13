f6401acbc39d640edd5edb74a509a8dae0349075
raheemjnr <mumedian6@gmail.com>
2025-02-13 17:10:21 +0100

    refactor: Clean up DrawinCanvas.kt
    Removes unused imports and commented-out code in DrawinCanvas.kt
    to improve code readability and maintainability. Specifically,
    removes `android.location.Location.distanceBetween` and other unused
    imports, as well as the commented-out `@Composable` function.

--- app/src/main/java/com/rjnr/thaiwrter/ui/drawing/DrawinCanvas.kt
+++ app/src/main/java/com/rjnr/thaiwrter/ui/drawing/DrawinCanvas.kt
@@ -1,6 +1,5 @@
 package com.rjnr.thaiwrter.ui.drawing
 
-import android.location.Location.distanceBetween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
@@ -14,19 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 import com.rjnr.thaiwrter.data.models.Point
 import com.rjnr.thaiwrter.data.models.ThaiCharacter
-import kotlin.math.PI
-import kotlin.math.atan2
-import kotlin.math.cos
 import kotlin.math.pow
-import kotlin.math.sin
 import kotlin.math.sqrt
 
 //@Composable
